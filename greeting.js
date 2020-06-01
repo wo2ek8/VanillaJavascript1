6c1782d34ef1d7d4e214c9e13f15d9324705b906
손재이 <jae2son@gmail.com>
2020-06-01 14:02:08 +0900

    Making a To Do List part One

--- greeting.js
+++ greeting.js
@@ -5,14 +5,26 @@ const greeting = document.querySelector('.js-greetings');
 const userLs = 'currentUser';
 const showingOn = 'showing';
 
+function saveName(text) {
+    localStorage.setItem(userLs, text);
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const cv = input.value;
+    paintGreeting(cv);
+}
+
 function askForName() {
     form.classList.add(showingOn);
+    form.addEventListener('submit', handleSubmit);
 }
 
 function paintGreeting(text) {
     form.classList.remove(showingOn);
     greeting.classList.add(showingOn);
     greeting.innerText = `Hello ${text}`;
+    saveName(text);
 }
 
 function loadName() {
@@ -22,7 +34,7 @@ function loadName() {
         console.log('currentUser === null');
     } else {
         paintGreeting(currentUser);
-        consoloe.log('currentUser !== null');
+        console.log('currentUser !== null');
     }
 }
 
