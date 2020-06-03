4467a298adfa306f8076b709a2d154e77522bfb6
손재이 <jae2son@gmail.com>
2020-06-03 13:47:10 +0900

    localStorage

--- clock.js
+++ clock.js
@@ -1,14 +1,25 @@
 const clockContainer = document.querySelector('.js-clock');
 const clockTitle = clockContainer.querySelector('h1');
 
+function getDate() {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const today = date.getDate();
+    const day = date.getDay();
+    const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+    
+    
+    
+    clockTitle.before(`${month < 10 ? '0' + month : month}월 ${today < 10 ? '0' + today : today}일 ${daysOfWeek[day]}요일`);
+}
+
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
+    
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-    
-    
     
     /*const name = '형진';
     const age = '24';
@@ -21,6 +32,7 @@ function getTime() {
 
 
 function init() {
+    getDate();
     getTime();
     setInterval(getTime, 1000);
 }
--- todo.js
+++ todo.js
@@ -4,17 +4,53 @@ const toDoList = document.querySelector('.js-toDoList');
 
 const TODOS_LS = 'toDos';
 
+
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     //console.log(text);
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
     delBtn.innerText = '❌';
+    delBtn.addEventListener('click', deleteToDo);
+    
     const span = document.createElement('span');
+    const newId = toDos.length + 1;
+    
     span.innerText = text;
+    
+    
     li.appendChild(span);
     li.appendChild(delBtn);
     //console.log(li);
+    li.id = newId;
+    
     toDoList.appendChild(li);
+    
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    
+    toDos.push(toDoObj);
+    
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -25,9 +61,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-        
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
