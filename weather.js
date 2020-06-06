a35316b34b3ce9b39bea5502d3e93ad80ca95aa8
손재이 <jae2son@gmail.com>
2020-06-06 16:26:09 +0900

    getWeather

--- weather.js
+++ weather.js
@@ -1,5 +1,17 @@
+const weather = document.querySelector('.js-weather');
+
 const API_KEY = '846e07cbcc3b6b4845b1be8ca51e8c7c';
 const COORDS = 'coords';
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +25,7 @@ function hadleGeoSucces(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function hadleGeoError() {
@@ -24,11 +37,12 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
         askForCoords();
     } else {
-        
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
@@ -37,3 +51,12 @@ function init() {
 }
 
 init();
+
+/*const linoo = '민우';
+let age = '31';
+const gender = '남자';
+
+
+const greet = `안녕! 나는 ${linoo}고 ${age}살이고 ${gender}야!`;
+
+console.log(greet);*/
